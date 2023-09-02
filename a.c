ed60660767b37acd851c1af828bd389fa48f0122
AmanStarLitePro <143260479+AmanStarLitePro@users.noreply.github.com>
2023-09-02 17:00:12 +0530

    Update a.c

--- a.c
+++ a.c
@@ -21,10 +21,10 @@ int main(){
   printf("Enter another Number : \n");
   scanf("%d", &b);
 
-  printf("The Addition of %d and %d is : %d", a, b , add(a,b));
-  printf("The Subtraction of %d and %d is : %d", a, b , subtract(a,b));
-  printf("The Multiplication of %d and %d is : %d", a, b , multiplication(a,b));
-  printf("The Division of %d and %d is : %d", a, b , divide(a,b));
+  printf("\n The Addition of %d and %d is : %d", a, b , add(a,b));
+  printf("\n The Subtraction of %d and %d is : %d", a, b , subtract(a,b));
+  printf("\n The Multiplication of %d and %d is : %d", a, b , multiplication(a,b));
+  printf("\n The Division of %d and %d is : %d", a, b , divide(a,b));
 
   return 0;
 }
